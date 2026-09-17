131b56248a5a3a96fc1817fb015ae0e09839f069
shahriarxproxima <shahriarxproximalog1@gmail.com>
2026-09-18 04:16:20 +0600

    Singleton with thread-safe

--- src/SingletonWithThreadSafe.java
+++ src/SingletonWithThreadSafe.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class SingletonWithThreadSafe {
+    private static volatile SingletonWithThreadSafe instance;
+    private String data;
+
+    private SingletonWithThreadSafe(String data) {
+        this.data = data;
+    }
+
+    public static SingletonWithThreadSafe getInstance(String data) {
+        if (instance == null) {
+            synchronized (SingletonWithThreadSafe.class) {
+                if (instance == null) {
+                    instance = new SingletonWithThreadSafe(data);
+                }
+            }
+
+        }
+        return instance;
+    }
 }
