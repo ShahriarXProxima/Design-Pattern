b5ece65718bd17fe99b421afbe4729bcc6b7c32b
shahriarxproxima <shahriarxproximalog1@gmail.com>
2026-09-23 21:12:53 +0600

    Simple factory

---
+++ src/Factory/SimpleFactory/BasicBurger.java
@@ -0,0 +1,8 @@
+package Factory.SimpleFactory;
+
+public class BasicBurger extends Burger {
+    @Override
+    void prepare(String s) {
+        super.prepare("Prepare a basic Burger");
+    }
+}
---
+++ src/Factory/SimpleFactory/Burger.java
@@ -0,0 +1,8 @@
+package Factory.SimpleFactory;
+
+public class Burger {
+    void prepare(String s) {
+        s = "Prepare a burger";
+        System.out.println(s);
+    }
+}
